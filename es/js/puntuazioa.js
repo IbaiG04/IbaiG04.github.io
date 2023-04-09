49bc0ec9dc243bbf9171507b7b5af9cf9912fe81
IbaiG04 <ibaigm04@gmail.com>
2023-04-09 14:30:08 +0200

    asd
    asd

--- es/js/puntuazioa.js
+++
@@ -1,7 +0,0 @@
-let boton = document.getElementById("container-like")
-
-boton.addEventListener("click", puntuazioa);
-
-function puntuazioa(){
-    document.getElementById("puntuazioa").innerHTML("Hola");
-}
