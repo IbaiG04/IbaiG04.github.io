ff9ca5145da53f464756e5271973d88ea19453da
IbaiG04 <97726677+IbaiG04@users.noreply.github.com>
2023-04-17 09:49:16 +0200

    JS DONE

---
+++ js/imgfrom.js
@@ -0,0 +1,7 @@
+const image = document.querySelector(".imgform"),
+    input = document.querySelector("#pfp");
+    
+
+input.addEventListener("change", () => { 
+    image.src = URL.createObjectURL(input.files[0]);
+});
